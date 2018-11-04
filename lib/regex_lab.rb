543e46f03a35bc1f28ff45344e599c39aef8c660
Fer Hernandez <fh.1111@gmail.com>
2018-11-04 04:03:28 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,7 +19,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if ((text.match(/^[A-Z]/) == nil) && (text.match(/[a-z]+\.$/) == nil))
+  if ((text.match(/^[A-Z]/) == nil) || (text.match(/[a-z]+\.$/) == nil))
     return false
   else
     return true
